7d818b1d7a8eb05e72af0b9abff53ce69e27777a
SzachMatH <szach.bart@gmail.com>
2025-10-31 20:27:21 +0100

    Added nvim config

---
+++ .config/nvim/nvim/lua/settings.lua
@@ -0,0 +1,29 @@
+local global = vim.g
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.syntax = "on"
+vim.opt.autoindent = true
+vim.opt.cursorline = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 3
+vim.opt.tabstop = 3
+vim.opt.encoding = "UTF-8"
+vim.opt.splitright = true
+vim.opt.ruler = true
+--vim.opt.mouse = "a"
+--vim.opt.title = true
+--vim.opt.hidden = true
+--vim.opt.ttimeoutlen = 0
+--vim.opt.wildmenu = true
+--vim.opt.showcmd = true
+--vim.opt.showmatch = true
+--vim.opt.inccommand = "split"
+--vim.opt.splitright = true
+--vim.opt.splitbelow = true
+--vim.opt.termguicolors = true
+vim.diagnostic.config({
+   virtual_text = true, 
+   virtual_lines = false,
+})
